5fc9946d4addde39969574ea600db06560e75fe0
Owen Bell <owenthebell@protonmail.com>
2024-08-13 13:23:21 -0400

    Created TorchLight to handle a time scaling light source
    TorchLight extends PointLight2D and provides timed adjustments to radius and light energy

---
+++ Player/Lights/TorchLight.gd
@@ -0,0 +1,37 @@
+class_name TorchLight extends PointLight2D
+
+signal on_extinguish
+
+@export var duration: float = 100
+@export_range(0.0, 1.0) var minimum_light: float = 1.0
+@export_range(0.0, 1.0) var minimum_radius: float = 1.0
+
+var remaining_time
+var is_lit = false
+
+var starting_scale
+var starting_energy
+
+func _ready():
+    is_lit = true
+    remaining_time = duration
+    starting_scale = texture_scale
+    starting_energy = energy
+    
+
+func on_tick(delta):
+    if not is_lit:
+        return
+    remaining_time -= delta
+    var percent_radius = (1 - minimum_radius) * (remaining_time / duration) + minimum_radius
+    texture_scale = starting_scale * percent_radius
+    var percent_energy = (1 - minimum_light) * (remaining_time / duration) + minimum_light
+    energy = starting_energy * percent_energy
+
+
+func ignite():
+    pass
+
+
+func extinguish():
+    pass
