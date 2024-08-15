4f48162b033ff8e6901c7437d6a4628891a8d51f
Owen Bell <owenthebell@protonmail.com>
2024-08-15 15:15:31 -0400

    InteractReceiver registers itself with InteractExecutor
    InteractReceivers handle calling InteractExecutor.add_receiver() and remove_receiver() as this opens up the flexibility to register or deregister themselves. This also removes the problem where they had to have the specific name to function.

--- Interaction/InteractExecutor.gd
+++ Interaction/InteractExecutor.gd
@@ -12,7 +12,7 @@ var elapsed_hold: float = 0
 var executing = false
 
 func _ready():
-  update_description()
+  _update_description()
 
 func _process(delta):
   if Input.is_action_just_pressed("Interact") and not executing and receivers.size() > 0:
@@ -29,47 +29,39 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-  var node = body.find_child("InteractReceiver")
-  if is_instance_valid(node):
-    add_receiver(node, true)
+  body.propagate_call("entered_executor", [self])
 
 
 func _on_body_exited(body):
-  var node = body.find_child("InteractReceiver")
-  if is_instance_valid(node):
-    remove_receiver(node)
+  body.propagate_call("exited_executor", [self])
 
 
 func _on_area_entered(area: Area2D):
-  var receiver = area.get_parent().find_child("InteractReceiver")
-  if is_instance_valid(receiver):
-    add_receiver(receiver)
+  area.owner.propagate_call("entered_executor", [self])
 
 
 func _on_area_exited(area: Area2D):
-  var receiver = area.get_parent().find_child("InteractReceiver")
-  if is_instance_valid(receiver):
-    remove_receiver(receiver)
+  area.owner.propagate_call("exited_executor", [self])
 
 
-func add_receiver(receiver: Node, priority = false):
+func add_receiver(receiver: Node, priority = true):
   if receivers.find(receiver) >= 0:
     return
   if priority:
     receivers.append(receiver)
   else:
     receivers.insert(0, receiver)
-  update_description()
+  _update_description()
 
 
 func remove_receiver(receiver: Node):
   var index = receivers.find(receiver)
   if index >= 0:
     receivers.remove_at(index)
-  update_description()
+  _update_description()
 
 
-func update_description():
+func _update_description():
   if receivers.size() > 0:
     description_var.value = receivers[receivers.size() - 1].interact_description
     has_interaction_var.value = true
--- Interaction/InteractReceiver.gd
+++ Interaction/InteractReceiver.gd
@@ -4,5 +4,18 @@ signal on_interact
 
 @export var interact_description: String
 
+var _executor: InteractExecutor
+
 func interact():
   on_interact.emit()
+
+
+func entered_executor(executor: InteractExecutor):
+  _executor = executor
+  executor.add_receiver(self)
+  pass
+
+
+func exited_executor(executor: InteractExecutor):
+  executor.remove_receiver(self)
+  pass
